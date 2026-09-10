b8df2a4a10ae4169ac34d23f591c95f7043e93ab
Seok_Soo <whdtn7855@naver.com>
2026-09-10 16:14:20 +0900

    chore: first commit

---
+++ src/main/java/com/getddo/GetddoBeApplication.java
@@ -0,0 +1,13 @@
+package com.getddo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetddoBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetddoBeApplication.class, args);
+	}
+
+}
